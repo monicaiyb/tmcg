591cb8f26a0e7e452bc2f019978ba4f00ad0dec7
unknown <monicaiyb@gmail.com>
2021-01-02 06:45:22 +0300

    Commit files for TMCG test

---
+++ README.md
@@ -0,0 +1,1 @@
+<!-- TMCG Practical test -->
---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const bodyParser= require('body-parser');
+const path=require("path");
+require('dotenv').config();
+// Import files from another others
+const routes =require("./routes/route");
+// initialise express to be used for 
+const app=express();
+// Setting the engine pug to be used to view our html files
+app.set("view engine","pug");
+app.set("views",path.join(__dirname,"views"));
+
+// Using our middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use("/",routes);
+
+
+
+// created a server have it listen at port 3000
+app.listen(3000,()=>console.log("Listening at port 3000"));
