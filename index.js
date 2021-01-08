3d035fd45aacecef67e6ac97fc000617e1ec6286
unknown <monicaiyb@gmail.com>
2021-01-08 11:55:44 +0300

    changes in all folders

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path=require("path");
 require('dotenv').config();
 // Import files from another others
 const routes =require("./routes/route");
+
 // initialise express to be used for 
 const app=express();
 // Setting the engine pug to be used to view our html files
@@ -15,9 +16,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname,"public")));
 
-app.use("/",routes);
+// app.use("/",routes);
 
-
+app.get("/", (req, res) => {
+    res.send("Hello World")
+});
 
 // created a server have it listen at port 3000
 app.listen(3000,()=>console.log("Listening at port 3000"));
---
+++ routes/route.js
@@ -0,0 +1,8 @@
+// Importing modules for use in the routes
+const express = require("express");
+const router = express.Router();
+// Require the database models to be used by the routes
+const db = require("../models/database");
+
+
+module.exports = router;
